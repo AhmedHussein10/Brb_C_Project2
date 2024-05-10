ee9cca9874523b8c51b419c95701ba177f3788ad
Ahmed <ahmad21hussein@gmail.com>
2024-05-11 02:55:33 +0300

    removing deleted files

--- Data Access_Layer/Data_Management_Module/data_management.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by HP on 5/5/2024.
-//
-
-#ifndef UNTITLED1_DATA_MANAGEMENT_H
-#define UNTITLED1_DATA_MANAGEMENT_H
-void editfunction(int);
-
-#endif //UNTITLED1_DATA_MANAGEMENT_H
--- input.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include "input.h"
-
-record students[100]; // Definition of the global array
-char Admin_Pass[20]="1234";
-
